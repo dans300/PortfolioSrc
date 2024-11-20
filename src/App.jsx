2a6241cec53e9552c4bcf21b0f31753ff6d8c4cc
dans300 <danielsierramejia@gmail.com>
2024-11-20 23:46:16 +0100

    starting with the header of the frotn page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
+import {About, Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from './components';
+import './index.css'; // or './tailwind.css'
 
-
-const App=()=> {
-  
-
+const App = () => {
   return (
-      <div>
-       something in the way 
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />   
+        </div>
+       <About />
+       <Experience />
+       <Tech />
+       <Works />
+       <Feedbacks />
+       <div className="relative z-0">
+        <StarsCanvas />
+        <Contact />
+        <StarsCanvas />
+       </div>
       </div>
+    </BrowserRouter>
     
   )
 }
