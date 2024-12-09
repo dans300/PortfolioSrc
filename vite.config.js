121641175d7acf4aeeec86ecc31e577f9fd64f54
dans300 <danielsierramejia@gmail.com>
2024-12-09 14:56:54 +0100

    Deploy to gh-pages 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react()],
-  base:"/PortfolioSrc/",
+  //base:"/Portfolio/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
